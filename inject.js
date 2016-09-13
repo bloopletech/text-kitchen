973f65998cdc759d60c4194611842794d7da2daa
Brenton Fletcher <i@bloople.net>
2016-09-14 01:41:17 +1000

    Add more selectors to match post text; Remove unused background.js file

--- inject.js
+++ inject.js
@@ -1,5 +1,5 @@
 function isMatch(node) {
-  if(node.matches("div.alt2, div.postcontent, p.message")) return true;
+  if(node.matches("div.alt2, div.postcontent, p.message, div.entry-content, div.boxbody")) return true;
   return false;
 }
 
