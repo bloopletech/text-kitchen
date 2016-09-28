0dad5bc9d36b16710978aaef56234886ce1cf084
Brenton Fletcher <i@bloople.net>
2016-09-28 19:12:40 +1000

    Clean up code style; Document match selectors

--- inject.js
+++ inject.js
@@ -1,11 +1,41 @@
-function isMatch(node) {
-  if(node.matches("div.alt2, div.postcontent, p.message, div.entry-content, div.boxbody, div.entry-inner")) return true;
-  return false;
+function compileMatchSelector() {
+  var selectors = [];
+  
+  selectors.push("textarea");
+  selectors.push("div.alt2"); //Contents of spoiler sections on some vBulletin forum posts
+  selectors.push("div.postcontent"); //vBulletin forum posts
+  selectors.push("p.message"); //kusaba-based imageboard posts
+  selectors.push("div.entry-content"); //?
+  selectors.push("div.boxbody"); //?
+  selectors.push("div.entry-inner"); //Some wordpress blogs
+  selectors.push("div#selectable"); //pastebin.com content
+
+  return selectors.join(", ");
 }
+
+var matchSelector = compileMatchSelector();
+
+var notificationStyles = `
+  box-sixing: border-box;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  z-index: 9999999;
+  padding: 15px;
+  border: 1px solid #d6e9c6;
+  border-radius: 4px;
+  color: #3c763d;
+  background-color: #dff0d8;
+  font-weight: normal;
+  font-size: 14px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+`;
+
+var notificationElement = `<div style="${notificationStyles.replace(/\s+/g, " ")}">Selection copied ✓</div>`;
 
 function notifyUser() {
   var alert = document.createElement("div");
-  alert.innerHTML = '<div style="box-sixing: border-box; position: fixed; top: 20px; right: 20px; z-index: 9999999; padding: 15px; border: 1px solid #d6e9c6; border-radius: 4px; color: #3c763d; background-color: #dff0d8; font-weight: normal; font-size: 14px; font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;">Selection copied ✓</div>';
+  alert.innerHTML = notificationElement;
   document.body.appendChild(alert);
 
   setTimeout(function() {
@@ -19,7 +49,7 @@ document.body.addEventListener("click", function(event) {
   var target = event.target;
   var matched = false;
   while(target) {
-    if(isMatch(target)) {
+    if(target.matches(matchSelector)) {
       matched = true;
       break;
     }
