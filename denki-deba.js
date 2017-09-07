f25369430ffca2d864c7905ef7c5d4d3d4f7f1eb
Brenton Fletcher <i@bloople.net>
2017-09-07 20:55:25 +1000

    Initial commit

---
+++ denki-deba.js
@@ -0,0 +1,9 @@
+var denkiDeba = (function() {
+  
+  
+  return function(input) {
+    //...
+  };
+})();
+
+if(typeof module === "object" && typeof module.exports === "object") module.exports = denkiDeba;
